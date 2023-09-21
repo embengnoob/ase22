2752318e70e2be648b954eaa21adeca2cbd30474
embengnoob <saeedaraam@gmail.com>
2023-09-21 14:08:01 +0200

    Open simulator window maximized

--- drive.bat
+++ drive.bat
@@ -1,4 +1,4 @@
 @echo off
 call activate udacity-self-driving-car
-start C:\ThirdEye\ase22\udacity-simulator\udacity-sim-win64\self_driving_car_nanodegree_program.exe
+start /MAX C:\ThirdEye\ase22\udacity-simulator\udacity-sim-win64\self_driving_car_nanodegree_program.exe
 python %~dp0\self_driving_car_drive.py
